ee4512f066df65dec239bb23f6cd6dfe8b13d7de
andriikonoh <andriikon@tahometer.com>
2019-03-12 18:13:11 +0200

    new message

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var list = document.querySelector('ul');
 list.addEventListener('click', function (ev) {
-    if(ev.target.tagName === "LI") {
+    if(ev.target.tagName == "LI") {
        ev.target.classList.toggle('checked');
-    } else if(ev.target.tagName === "BUTTON") {
-       var div = ev.target.parentNode;
+    } else if(ev.target.className == "spanDel") {
+      var div = ev.target.parentNode;
        div.remove();
     }
-	else if(ev.target.tagName === "SPAN"){
+	else if(ev.target.className =="spanEdit"){
 		var txtChng2 = prompt('Change Task', ev.target.parentNode.firstChild.nodeValue);
 		ev.target.parentNode.firstChild.nodeValue = txtChng2;
-		//console.log(ev.target.parentNode.firstChild.nodeValue);
-		//ev.target.replaceChild(toDoEl, txtChng2);
+		console.log(ev.target.parentNode.firstChild.nodeValue);
+		ev.target.replaceChild(toDoEl, txtChng2);
 	}
 }, false);
 
@@ -25,12 +25,12 @@ function newElement() {
        document.getElementById('list').appendChild(li);
     }
     document.getElementById('toDoEl').value = "";
-    var btn = document.createElement('BUTTON');
-    var txt = document.createTextNode("X");
-	var span = document.createElement('SPAN');
-	var txtChng = document.createTextNode("     Edit");
-	span.className = "replace";
-    btn.className = "close";
+    var btn = document.createElement('spanDel');
+    var txt = document.createTextNode(" X");
+    var span = document.createElement('spanEdit');
+	var txtChng = document.createTextNode(" Edit");
+	span.className = "spanEdit";
+    btn.className = "spanDel";
 	span.appendChild(txtChng);
 	li.appendChild(span);
     btn.appendChild(txt);
