b173eb4eb3b88102329f150b7a7f9baa71d97f29
andriikonoh <andriykonogray@gmail.com>
2019-03-18 01:29:28 +0200

    finish all functional and start server

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var static = require('node-static');
+var file = new static.Server('.');
+
+http.createServer(function(req, res) {
+    file.serve(req, res);
+}).listen(3000);
+
+console.log('Server running on port 3000');
+
