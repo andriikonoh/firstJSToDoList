0aaccd6b1d08bc5919d5d6462e24c64431960e76
andriikonoh <48058172+andriikonoh@users.noreply.github.com>
2019-03-19 15:15:36 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,18 +2,24 @@ list.addEventListener('click', clickOnTask, false);
 
 project.addEventListener("click", clickOnProject, false);
 
+document.addEventListener("DOMContentLoaded", function() {
+    renderProjects();
+    renderTasks();
+});
+
 let projects = [];
 let tasks = [];
-
-
+let selectedProject;
 
 function clickOnTask (event) {
     if(event.target.className === "close") {
-        event.target.parentElement.remove();
+        deleteTask(event.target.parentElement.id);
     }
-    else if(event.target.className === "replace"){
-        event.target.parentElement.firstChild.innerText =
-            prompt('Change Task', event.target.parentElement.firstChild.textContent);;
+    else if(event.target.className === "replace") {
+        let task = event.target.parentElement;
+        task.name = prompt('Change Task', event.target.parentElement.firstChild.textContent);
+        task.firstChild.innerText = task.name;
+        putTask(task);
     }
     else {
         event.target.classList.toggle('checked');
@@ -21,19 +27,20 @@ function clickOnTask (event) {
     }
 }
 
-let selectedProject;
 function clickOnProject (event) {
     if(event.target.className === "close") {
-        event.target.parentElement.remove();
+        deleteProject(selectedProject.id);
         for(let i = 0; i<tasks.length; i++) {
-                if(tasks[i].projectId === selectedProject.name) {
-                    tasks[i].remove();
+                if(tasks[i].projectId === selectedProject.id) {
+                    deleteTask(tasks[i].id);
                 }
         }
     }
-    else if(event.target.className === "replace"){
-        event.target.parentElement.firstChild.innerText =
-            prompt('Change Task', event.target.parentElement.firstChild.textContent);;
+    else if(event.target.className === "replace") {
+        let project = event.target.parentElement;
+        project.name = prompt('Change Task', event.target.parentElement.firstChild.textContent);
+        project.firstChild.innerText = project.name;
+        putProject(project);
     }
     else{
         select(event.target.parentElement);
@@ -46,8 +53,8 @@ function select(node) {
     }
     selectedProject = node;
     selectedProject.firstChild.classList.add("selected");
-    for(let i = 0; i<tasks.length; i++){
-        if (tasks[i].projectId != selectedProject.name) {
+    for(let i = 0; i < tasks.length; i++){
+        if (tasks[i].projectId != selectedProject.id) {
             tasks[i].hidden = true;
         }
         else {
@@ -56,56 +63,29 @@ function select(node) {
     }
 }
 
-function newElement() {
+function newTask() {
     let li = document.createElement('li');
-    let inputValue = document.getElementById('toDoEl').value;
-    let t = document.createElement('span');
-    t.innerText = inputValue;
-    li.appendChild(t);
-    if(inputValue == "") {
-       alert("AddTask");
+    li.name = document.getElementById('toDoEl').value;
+    li.id = tasks.length+1;
+    li.projectId = selectedProject.id;
+    if(li.name == "") {
+        alert("AddTask");
     } else {
-       document.getElementById('list').appendChild(li);
+        taskFactory(li);
+        setTask(JSON.stringify(li));
     }
-    document.getElementById('toDoEl').value = "";
-    let btnClose = document.createElement('span');
-	let btnEdit = document.createElement('span');
-    btnEdit.className = "replace";
-    btnClose.className = "close";
-    btnEdit.innerText = " Edit";
-	li.appendChild(btnEdit);
-    btnClose.innerText = " X";
-    li.appendChild(btnClose);
-    li.projectId = selectedProject.name;
-    li.name = t.innerText;
-    setTask(JSON.stringify(li))
-    tasks.push(li);
 }
 function newProject() {
     let li = document.createElement('li');
-    let inputValue = document.getElementById('toDoProj').value;
-    let t = document.createElement('span');
-    t.innerText = inputValue;
-    t.className = "ProjUnDone";
-    li.appendChild(t);
-    if(inputValue == "") {
+    li.name = document.getElementById('toDoProj').value;
+    li.id = projects.length+1;
+    if(li.name == "") {
         alert("AddProject");
     } else {
-        document.getElementById('project').appendChild(li);
+        projectFactory(li);
+        projects.push(li);
+        setProject(JSON.stringify(li));
     }
-    document.getElementById('toDoProj').value = "";
-    let btnClose = document.createElement('span');
-    let btnEdit = document.createElement('span');
-    btnEdit.className = "replace";
-    btnClose.className = "close";
-    btnEdit.innerText = " Edit";
-    li.appendChild(btnEdit);
-    btnClose.innerText = " X";
-    li.appendChild(btnClose);
-    li.name = t.innerText;
-    projects.push(li);
-    setProject(JSON.stringify(li));
-    select(li);
 }
 
  function setProject(data) {
@@ -116,7 +96,27 @@ function newProject() {
         body: data,
         headers: headers
     });
-} 
+}
+
+function putProject(project) {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return fetch(`http://localhost:3000/projects/${project.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(project),
+        headers: headers
+    }, {name: name})
+}
+
+function putTask(task) {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return fetch(`http://localhost:3000/tasks/${task.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(task),
+        headers: headers
+    }, {name: name})
+}
 
 function getProjects() {
     return fetch("http://localhost:3000/projects")
@@ -138,6 +138,27 @@ function setTask(data) {
     });
 }
 
+function deleteProject(id) {
+    id = selectedProject.id;
+    return fetch(`http://localhost:3000/projects/${id}`, {
+        method: 'DELETE'
+    })
+        .then( function() {
+            renderProjects();
+            renderTasks();
+        })
+}
+
+function deleteTask(id) {
+    return fetch(`http://localhost:3000/tasks/${id}`, {
+        method: 'DELETE'
+    })
+        .then( function() {
+            renderProjects();
+            renderTasks();
+        })
+}
+
 function getTasks() {
     return fetch("http://localhost:3000/tasks")
     .then(function(response) {
@@ -151,71 +172,74 @@ function getTasks() {
 function renderTasks() {
     getTasks()
     .then(function(tasksDTO) {
-        for(let i = 0;i<tasks.length;i++) {
+        for(let i = 0; i < tasks.length; i++) {
             tasks.pop();
         }
         document.getElementById('list').innerHTML = "";
         tasks = JSON.parse(tasksDTO);
-        alert(JSON.stringify(tasks));
         let length = tasks.length;
-        for(let i = 0; i<length; i++) {
-            let li = document.createElement('li');
-            let t = document.createElement('span');
-            t.innerText = tasks[i].name;
-            li.appendChild(t);
-            document.getElementById('list').appendChild(li);
-            document.getElementById('toDoEl').value = "";
-            let btnClose = document.createElement('span');
-            let btnEdit = document.createElement('span');
-            btnEdit.className = "replace";
-            btnClose.className = "close";
-            btnEdit.innerText = " Edit";
-            li.appendChild(btnEdit);
-            btnClose.innerText = " X";
-            li.appendChild(btnClose);
-            li.projectId = tasks[i].projectId;
-            li.name = tasks[i].name;
-            tasks.push(li);
+        for(let i = 0; i < length; i++) {
+            taskFactory(tasks[i]);
         }
     })
-    .then( function() {
-        select(selectedProject);
-    })
-    .catch(console.error);
+        .catch(console.error);
 }
 
 function renderProjects() {
     getProjects()
     .then(function(projectsDTO) {
-        for(let i = 0; i<projects.length; i++) {
+        for(let i = 0; i < projects.length; i++) {
             projects.pop();
         }
         document.getElementById('project').innerHTML = "";
         projects = JSON.parse(projectsDTO);
         let length = projects.length;
-        for(let i = 0;i<length;i++) {
-            let li = document.createElement('li');
-            let t = document.createElement('span');
-            t.innerText = projects[i].name;
-            t.className = "ProjUnDone";
-            li.appendChild(t);
-            document.getElementById('project').appendChild(li);
-            document.getElementById('toDoProj').value = "";
-            let btnClose = document.createElement('span');
-            let btnEdit = document.createElement('span');
-            btnEdit.className = "replace";
-            btnClose.className = "close";
-            btnEdit.innerText = " Edit";
-            li.appendChild(btnEdit);
-            btnClose.innerText = " X";
-            li.appendChild(btnClose);
-            li.name = projects[i].name; 
-            projects.push(li);
+        for(let i = 0; i < length; i++) {
+            projectFactory(projects[i]);
         }
     })
-    .then( function() {
-        select(selectedProject);
-    })
-    .catch(console.error);
+        .catch(console.error);
 }
 
+function projectFactory(project) {
+    ///////
+    let li = document.createElement('li');
+    let t = document.createElement('span');
+    t.innerText = project.name;
+    t.className = "ProjUnDone";
+    li.appendChild(t);
+    document.getElementById('project').appendChild(li);
+    document.getElementById('toDoProj').value = "";
+    let btnClose = document.createElement('span');
+    let btnEdit = document.createElement('span');
+    btnEdit.className = "replace";
+    btnClose.className = "close";
+    btnEdit.innerText = " Edit";
+    li.appendChild(btnEdit);
+    btnClose.innerText = " X";
+    li.appendChild(btnClose);
+    li.name = project.name;
+    li.id = project.id;
+    select(li);
+}
+
+function taskFactory(task) {
+    let li = document.createElement('li');
+    let t = document.createElement('span');
+    t.innerText = task.name;
+    li.appendChild(t);
+    document.getElementById('list').appendChild(li);
+    document.getElementById('toDoEl').value = "";
+    let btnClose = document.createElement('span');
+    let btnEdit = document.createElement('span');
+    btnEdit.className = "replace";
+    btnClose.className = "close";
+    btnEdit.innerText = " Edit";
+    li.appendChild(btnEdit);
+    btnClose.innerText = " X";
+    li.appendChild(btnClose);
+    li.projectId = task.projectId;
+    li.name = task.name;
+    li.id = task.id;
+    tasks.push(li);
+}
